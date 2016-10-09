f9d5411f0695f876e3ec97238ecfaaf99ee42501
Daykm <daykmsc@gmail.com>
2016-10-09 19:51:12 -0400

    Add some data and services

---
+++ app/src/main/java/com/example/daykm/daggerexample/data/remote/Coordinate.java
@@ -0,0 +1,11 @@
+package com.example.daykm.daggerexample.data.remote;
+
+import com.squareup.moshi.Json;
+
+public class Coordinate {
+
+    @Json(name = "lon")
+    public float lon;
+    @Json(name = "lat")
+    public float lat;
+}
